98d376da17399af38e226384928c40ac4a48e9ee
arashi walker and Tanvi Garg <tanvi@>
2018-10-18 16:35:27 -0700

    add if else statements for selecting the correct kind of triangle based on the input values

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,40 @@
 var side1Input;
 var side2Input;
 var side3Input;
-var sum1 = side1Input + side2Input;
-var sum2 = side2Input + side3Input;
-var sum3 = side1Input + side3Input;
 
 function choose(){
-  // if (ageInput > 20 && ageInput) {
-  //   $("#match").show();
-  //   $("#name").text(nameInput);
-  //   $("#unmatch").hide();
-    if(side1Input === side2Input && side1Input === side3Input && side2Input === side3Input) {
-      $("#equiresult").show();
-      $("#isoresult").hide();
-      $("#scaresult").hide();
-      $("#notresult").hide();
-      }
-    else if(side1Input === side2Input || side2Input === side3Input || side1Input === side3Input) {
-      $("#isoresult").show();
-      $("#equiresult").hide();
-      $("#scaresult").hide();
-      $("#notresult").hide();
-    }
-    else if(side1Input !== side2Input && side2Input !== side3Input && side1Input !== side3Input) {
-      $("#scaresult").show();
-      $("#equiresult").hide();
-      $("#isoresult").hide();
-      $("#notresult").hide();
-    }
-    else if(sum1 <= side3Input || sum2 <= side1Input || sum3 <= side2Input) {
+  var sum1 = side1Input + side2Input;
+  var sum2 = side2Input + side3Input;
+  var sum3 = side1Input + side3Input;
+    if (sum1 < side3Input || sum2 < side1Input || sum3 < side2Input) {
       $("#notresult").show();
       $("#equiresult").hide();
       $("#isoresult").hide();
       $("#scaresult").hide();
+      var notTri = true;
     }
-  }
-  // else if (ageInput < 20 && ageInput) {
-  //   $("#unmatch").show();
-  //   $("#name1").text(nameInput);
-  //   $("#match").hide();
-  //   $("#princessDate4").hide();
-  //   $("#princessDate1").hide();
-  //   $("#princessDate2").hide();
-  //   $("#princessDate3").hide();
-  // }
+        if(side1Input === side2Input && side1Input === side3Input && side2Input === side3Input) {
+          $("#equiresult").show();
+          $("#isoresult").hide();
+          $("#scaresult").hide();
+          $("#notresult").hide();
+        }
+        else if(side1Input === side2Input || side2Input === side3Input || side1Input === side3Input) {
+          $("#isoresult").show();
+          $("#equiresult").hide();
+          $("#scaresult").hide();
+          $("#notresult").hide();
+        }
+        else if(side1Input !== side2Input && side2Input !== side3Input && side1Input !== side3Input && !notTri) {
+          $("#scaresult").show();
+          $("#equiresult").hide();
+          $("#isoresult").hide();
+          $("#notresult").hide();
+        }
+
+
+    }
+
 
 
 
